3350b1fa1c610b41ad94c1c52e119eeca4c276f2
Sami Jakonen <sami.jakonen@iki.fi>
2021-02-28 20:20:59 +0200

    Implement a way to save data to backend

--- discord-voice-bot-web.js
+++ discord-voice-bot-web.js
@@ -1,42 +1,75 @@
 const express = require('express');
+const cors = require('cors')
+const fs = require('fs');
+const bodyParser = require('body-parser')
+
+
+var jsonParser = bodyParser.json()
 const app = express();
+app.use(cors());
 
 
-// Fake data from a "database"
-const PLAYLISTS =
-[
-    { id: "Anime", songs: [
-        "https://www.youtube.com/watch?v=fvSRM1zFQ_0",
-        "https://www.youtube.com/watch?v=Db1fj2pRFu8",
-        "https://www.youtube.com/watch?v=WAoPeG1LU1g",
-        "https://www.youtube.com/watch?v=nU21rCWkuJw",
-    ]},
-    { id: "Raikku", songs: [
-        "https://www.youtube.com/watch?v=Y1HIDtGZlXI",
-    ]},
-    { id: "STP", songs: [
-        "https://www.youtube.com/watch?v=iQ6iNkqmk6M",
-        "https://www.youtube.com/watch?v=lg1_OYFLgHU",
-        "https://www.youtube.com/watch?v=Bl-ouDbqy-g",
-        "https://www.youtube.com/watch?v=s8LsZNITPqk",
-        "https://www.youtube.com/watch?v=ab2lUr3hNIk",
-        "https://www.youtube.com/watch?v=sS4JY2JOwZs",
-    ]},
-];
+// Read data file
+fetchPersistent = () => {
+    try {
+        // Read data synchronously
+        return fs.readFileSync("data/playlists.json", "utf8");
+    } catch {
+        return [];
+    }
+}
 
 
+// Write changes to data file
+writePersistent = (data) => {
+    try {
+        // Read data synchronously
+        fs.writeFileSync("data/playlists.json", data)
+        return true;
+    } catch {
+        return false;
+    }
+}
+
+
+// Entrypoint for getting saved playlists
+app.get("/fetch_data", (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    return res.send(fetchPersistent());
+});
+
+
+// Entrypoint for updating playlist data
+app.post("/update_data", jsonParser, (req, res) => {
+    // Combine the incoming playlists and already saved playlists
+    let updatedPlaylists = req.body;
+
+    /*
+    Currently this part relies on the front-end to have a copy of all
+    the desired information. TODO: do not replace all data with one
+    coming from the front-end but process the diff of playlists here
+    and combine the new data with old data. Below some experimental stuff.
+
+    let savedPlaylists = JSON.parse(fetchPersistent());
+    let combinedNewData = savedPlaylists.concat(updatedPlaylists)
+                            .filter((v, i, a) => a.findIndex(t => (t.id === v.id)) === i);
+    */
+
+    if (writePersistent(JSON.stringify(updatedPlaylists))) {
+        return res.send({ updated: true });
+    } else {
+        return res.send({ updated: false });
+    }
+});
+
+
+// Serve front-end
 app.get("/", (req, res) => {
     return res.send("Hello");
 });
 
 
-app.get("/fetch_data", (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.setHeader("Content-Type", "application/json");
-    return res.send(PLAYLISTS);
-});
-
-
+// Start server
 app.listen(20202, () =>
     console.log("Server listening"),
 );
